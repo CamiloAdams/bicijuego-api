a5a1aef9997f227dc417f622e128926e0bc58daf
Camilo Adames <camiloadames@gmail.com>
2022-10-19 21:23:27 -0500

    get users route

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import questionsRoutes from "./routes/questions.routes";
 import authRoutes from "./routes/auth.routes";
 import guidesRoutes from "./routes/guides.routes";
 import scoreRoutes from "./routes/score.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 
@@ -32,4 +33,5 @@ app.use("/api/questions", questionsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/guides", guidesRoutes);
 app.use("/api/scores", scoreRoutes);
+app.use("/api/users", userRoutes);
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,10 @@
 import { Router } from "express";
+
+import * as userCtrl from "../controllers/user.controller";
+import { authjwt } from "../middlewares";
+
 const router = Router();
+
+router.get("/", [authjwt.verifyToken, authjwt.isAdmin], userCtrl.getUsers);
 
 export default router;
