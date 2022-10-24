4f5bbfc892b7681246d200133a00679ff530ed6d
Camilo Adames <camiloadames@gmail.com>
2022-10-24 11:03:48 -0500

    https test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import app from "./app";
+import fs from "fs";
+import http from "http";
+import https from "https";
+
 import "./database";
 
-app.listen(3000);
+const certificate = fs.readFile(
+    "./bicijuego.online/certificate.crt",
+    "utf8",
+    (err) => {
+        if (err) console.log(err);
+    }
+);
 
-console.log("Server listen on port:", 3000);
+const privateKey = fs.readFile(
+    "./bicijuego.online/private.key",
+    "utf8",
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3000);
+httpsServer.listen(3001);
+
+console.log("Http server listen port 3000");
+console.log("Https server listen port 3001");
+
+//app.listen(3000);
+
+//console.log("Server listen on port:", 3000);
